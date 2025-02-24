7e16a0742153b3d81d8d9ca54c5110c9d6e0fa76
pgimenez <pablo@saffire.com>
2025-02-24 18:49:27 -0300

    Se agrega base de datos de SQL Server con EF Core como ORM junto con algunos registros de prueba.

---
+++ AbmClientes/src/Infrastructure/Data/AbmClientesDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Aban.AbmClientes.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Aban.AbmClientes.Infrastructure.Data
+{
+    public class AbmClientesDbContext : DbContext
+    {
+        public AbmClientesDbContext(DbContextOptions<AbmClientesDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<Cliente>().HasData(
+                new Cliente
+                {
+                    Id = 1,
+                    Nombres = "Juan",
+                    Apellidos = "Pérez",
+                    FechaDeNacimiento = new DateOnly(1985, 5, 20),
+                    Cuit = "20123456789",
+                    Domicilio = "Calle Falsa 123",
+                    Celular = "1122334455",
+                    Email = "juan.perez@example.com"
+                },
+                new Cliente
+                {
+                    Id = 2,
+                    Nombres = "María",
+                    Apellidos = "González",
+                    FechaDeNacimiento = new DateOnly(1990, 10, 15),
+                    Cuit = "27345678901",
+                    Domicilio = "Av. Siempre Viva 742",
+                    Celular = "1198765432",
+                    Email = "maria.gonzalez@example.com"
+                },
+                new Cliente
+                {
+                    Id = 3,
+                    Nombres = "Mariano",
+                    Apellidos = "Gomez",
+                    FechaDeNacimiento = new DateOnly(1988, 5, 10),
+                    Cuit = "27349878901",
+                    Domicilio = "Av. Medrano 123",
+                    Celular = "1132165432",
+                    Email = "mariano.gomez@example.com"
+                }
+            );
+        }
+    }
+}
---
+++ AbmClientes/src/Infrastructure/Data/Config/ClienteConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Aban.AbmClientes.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aban.AbmClientes.Infrastructure.Data.Config
+{
+    public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            // Primary Key
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(c => c.Nombres).IsRequired().HasMaxLength(64);
+            builder.Property(c => c.Apellidos).IsRequired().HasMaxLength(64);
+            builder.Property(c => c.FechaDeNacimiento).IsRequired().HasColumnType("date");
+
+            builder.Property(c => c.Cuit)
+                .IsRequired()
+                .HasMaxLength(11)
+                .IsUnicode(false);
+
+            builder.Property(c => c.Domicilio).IsRequired().HasMaxLength(128);
+            builder.Property(c => c.Celular).IsRequired().HasMaxLength(32);
+
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(512)
+                .IsUnicode(false);
+
+            // Indexes
+            builder.HasIndex(c => c.Cuit)
+                .IsUnique()
+                .HasFilter("[Cuit] IS NOT NULL");
+
+            builder.HasIndex(c => c.Email)
+                .IsUnique()
+                .HasFilter("[Email] IS NOT NULL");
+        }
+    }
+}
